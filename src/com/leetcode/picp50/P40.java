6c5cf7297288669f061eeb3df71f8eb28df6f972
陳君豪 <justin.uob@gmail.com>
2022-03-10 22:24:49 +0800

    50 picp

--- src/com/leetcode/picp50/P40.java
+++ src/com/leetcode/picp50/P40.java
@@ -7,40 +7,32 @@ public class P40 {
 
     public static void main(String[] args) {
 
-        String num = "825563";
-        int k = 2;
+        String num = "10"; // "825563";
+        int k = 1; // 2
         String res = smallestAfterRemoving(num,k);
-        System.out.println(res);
+        System.out.println(res); // 2553
     }
 
-
-
     static String smallestAfterRemoving(String num, int k){
-        if(k==num.length())
+        if(num.length()==k)
             return "0";
 
         Stack<Integer> stack = new Stack<>();
 
-        for(char i : num.toCharArray()){ // 8 2 5 5 6 3
-            int currNum = i-'0';
-
-            while (k > 0 && !stack.isEmpty() && stack.peek() > currNum) { // 左側較大的數字打掉
+        for(char curr : num.toCharArray()){ // 1 0
+            int currNum = curr-'0';
+            // 左側大的數字敲掉
+            while(k>0 && !stack.isEmpty() && stack.peek()>currNum){
                 stack.pop();
                 k--;
             }
             stack.push(currNum);
         }
-
-        // 因為大的會在上面
+        // 大的在上，直接敲掉(剩下的k)
         while (k>0){
             stack.pop();
             k--;
         }
-
-//        for(int temp : stack){
-//            System.out.println(temp);
-//        }
-
 
         /*
         1
@@ -48,24 +40,67 @@ public class P40 {
         2
         0
         0
+
+        // 8 2 5 5 6 3
          */
-        // build the final string, while removing the leading zeros.
-        StringBuilder ret = new StringBuilder();
-        boolean leadingZero = true;
-        for(int digit: stack) { // 下到上取得
-            if(leadingZero && digit == 0) continue;
-            leadingZero = false; // 第一個數字非零即lock
-            ret.append(digit);
+        // 由下處理leading zero的問題
+        StringBuilder sb = new StringBuilder();
+        boolean lock = true;
+        for(Integer i : stack){
+            if(lock && i==0)
+                continue;
+
+            sb.append(i);
+            lock = false;
         }
 
-        /* return the final string  */
-        if (ret.length() == 0) return "0";
-        return ret.toString();
-
-
-
-
+        // 處理像10, 1 這樣的組合
+        if(sb.length()==0)
+            return "0";
+        return sb.toString();
     }
+
+
+//    static String smallestAfterRemoving(String num, int k){
+//        if(k==num.length())
+//            return "0";
+//
+//        Stack<Integer> stack = new Stack<>();
+//        for(char i : num.toCharArray()){ // 8 2 5 5 6 3
+//            int currNum = i-'0';
+//            while (k > 0 && !stack.isEmpty() && stack.peek() > currNum) { // 左側較大的數字打掉
+//                stack.pop();
+//                k--;
+//            }
+//            stack.push(currNum);
+//        }
+//
+//        // 因為大的會在上面
+//        while (k>0){
+//            stack.pop();
+//            k--;
+//        }
+//
+//        /*
+//        1
+//        0
+//        2
+//        0
+//        0
+//         */
+//        // build the final string, while removing the leading zeros.
+//        StringBuilder ret = new StringBuilder();
+//        boolean leadingZero = true;
+//        for(int digit: stack) { // 下到上取得
+//            if(leadingZero && digit == 0) continue;
+//            leadingZero = false; // 第一個數字非零即lock
+//            ret.append(digit);
+//        }
+//
+//        /* return the final string  */
+//        if (ret.length() == 0) return "0";
+//        return ret.toString();
+//    }
 }
 
 
---
+++ src/com/leetcode/picp50/P41.java
@@ -0,0 +1,75 @@
+package com.leetcode.picp50;
+
+import java.util.ArrayList;
+
+public class P41 {
+    /*
+    Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+
+    Output: [[1,5],[6,9]]
+     */
+
+    public static void main(String[] args) {
+//        int[][] int2DArr = new int[][]{{1,3},{6,9}};
+//        int[] intArr = new int[]{2,5};
+        int[][] int2DArr = new int[][]{{1,3},{7,10}};
+        int[] intArr = new int[]{4,6};
+        int[][] res = insertInterval(int2DArr,intArr);
+        System.out.println(res);
+    }
+
+
+    static int[][] insertInterval(int[][] intervals, int[] newInterval){
+        ArrayList<int[]> output = new ArrayList<>();
+        int i = 0;
+        // 前段
+        while(i<intervals.length && intervals[i][1] < newInterval[0]){
+            output.add(intervals[i]);
+            i++;
+        }
+
+        // 中段(one) 當前的頭<=newInterval的尾
+        while (i<intervals.length && intervals[i][0]<=newInterval[1]){
+            // merge的頭 & merge的尾
+            newInterval[0] = Math.min(intervals[i][0],newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1],newInterval[1]);
+            i++;
+        }
+        output.add(newInterval);
+
+        // 後段
+        while (i<intervals.length){
+            output.add(intervals[i]);
+            i++;
+        }
+
+        return output.toArray(new int[output.size()][2]);
+    }
+
+
+
+//    static int[][] insertInterval(int[][] intervals, int[] newInterval){
+//        ArrayList<int[]> output = new ArrayList<>();
+//        int i = 0;
+//        // 前段: newInterval 在curr的右側，這意味著沒有交集，curr直接放進去
+//        while(i < intervals.length && intervals[i][1] < newInterval[0]){
+//            output.add(intervals[i++]);
+//        }
+//
+//        // 中段: curr的頭 <= newInterval的尾，代表有交集: 頭取兩者之前，尾取兩者之後
+//        while(i < intervals.length && intervals[i][0] <= newInterval[1]){
+//            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+//            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+//            i++;
+//        }
+//        output.add(newInterval);
+//
+//        // 尾段
+//        while(i < intervals.length){
+//            output.add(intervals[i++]);
+//        }
+//
+//        // ArrayList<int[]> output
+//        return output.toArray(new int[output.size()][2]);
+//    }
+}
