c3c6c2c097b7e9a7c937e60fb186e7b4e728bc21
陳君豪 <justin.uob@gmail.com>
2022-03-24 21:06:31 +0800

    blind 5 6 7/75

---
+++ src/com/leetcode/blind75/P5_p1.java
@@ -0,0 +1,29 @@
+package com.leetcode.blind75;
+
+public class P5_p1 {
+    public static void main(String[] args) {
+        // {-2,1,-3,4,-1,2,1,-5,4} -> 6
+        // {5,4,-1,7,8}; // -> 23
+        int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        int res = maxSubArray(nums);
+        System.out.println(res);
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int max = nums[0];
+        int currSum = 0;
+        for (int i = 0; i<n; i++){
+            currSum = currSum + nums[i];
+            max = Math.max(max,currSum);
+            if(currSum<0){
+                currSum = 0;
+                continue;
+            }
+
+        }
+
+        return max;
+    }
+}
+
