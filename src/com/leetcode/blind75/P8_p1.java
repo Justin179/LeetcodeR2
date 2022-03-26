6f38b0b028ed38a2a31ced774f260fb405e66820
陳君豪 <justin.uob@gmail.com>
2022-03-27 00:10:08 +0800

    blind 8 9/75

---
+++ src/com/leetcode/blind75/P8_p1.java
@@ -0,0 +1,87 @@
+package com.leetcode.blind75;
+
+public class P8_p1 {
+
+    public static void main(String[] args) {
+//        int[] nums = new int[]{4,5,6,7,0,1,2};
+//        int target = 0; // 4
+//        int[] nums = new int[]{4,5,6,7,0,1,2};
+//        int target = 3; // -1
+//        int[] nums = new int[]{1};
+//        int target = 0; // -1
+//        int[] nums = new int[]{1,3};
+//        int target = 3;
+        int[] nums = new int[]{3,1};
+        int target = 3;
+        int res = search(nums, target);
+        System.out.println(res); // 0
+    }
+
+    /*
+        int[] nums = new int[]{4,5,6,7,0,1,2};
+        int target = 0; // 4
+     */
+    // 找到最小值: find the pivot point 這樣就知道要對該點的左邊或是右邊進行binary search了
+    public static int search(int[] nums, int target) {
+        int n = nums.length;
+        int minIndex = findMinIndex(nums);
+        int left = 0;
+        int right = n-1;
+
+        if(nums[minIndex]==target)
+            return minIndex;
+        else if (target>=nums[minIndex] && target<=nums[right])
+            left = minIndex;
+        else
+            right = minIndex-1;
+
+        while (left<right){
+            int mid = left + (right-left)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if (target>nums[mid])
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+        return nums[left]==target? left: -1;
+    }
+
+    public static int findMinIndex(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = n-1;
+        while (left<right){
+            int mid = left + (right-left)/2;
+            if(mid>0&&nums[mid-1]> nums[mid])
+                return mid;
+            else if (nums[left] <= nums[mid] && nums[mid] > nums[right])
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+        return left;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
