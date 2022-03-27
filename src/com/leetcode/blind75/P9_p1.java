a155fae5c41ad15dfde6c11c772cf97358d08665
陳君豪 <justin.uob@gmail.com>
2022-03-27 14:12:57 +0800

    blind 9 10/75

--- src/com/leetcode/blind75/P9_p1.java
+++ src/com/leetcode/blind75/P9_p1.java
@@ -4,49 +4,55 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class P9 {
+public class P9_p1 {
     /*
         Input: nums = [-1,0,1,2,-1,-4]
         Output: [[-1,-1,2],[-1,0,1]]
      */
     public static void main(String[] args) {
-//        int[] nums = new int[]{-1,0,1,2,-1,-4};
-        int[] nums = new int[]{-2,0,0,2,2};
+        int[] nums = new int[]{-1,0,1,2,-1,-4};
+//        int[] nums = new int[]{-2,0,0,2,2};
         List<List<Integer>> res = threeSum(nums);
         System.out.println(res);
     }
 
     // -4 -1 -1 0 1 2
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
         int n = nums.length;
         for (int i = 0; i<n-2; i++){
             int currNum = nums[i];
+
+            // 此輪上輪已經跑過，就 continue
             if(i>0 && currNum==nums[i-1])
                 continue;
 
             int j = i+1;
             int k = n-1;
-            while (j<k){
 
-                int sum = currNum + nums[j]+nums[k];
-                if ( sum == 0 ){
+            while (j<k){
+                int sum = currNum + nums[j] + nums[k];
+                if (sum==0){
                     res.add(Arrays.asList(currNum,nums[j],nums[k]));
+
+                    // 先預判下一個
                     while (j<k && nums[j]==nums[j+1])
                         j++;
-                    j++;
                     while (j<k && nums[k]==nums[k-1])
                         k--;
+
+                    j++;
                     k--;
-                } else if (sum > 0){
+                } else if (sum>0){
                     k--;
-                } else { // sum < 0
+                } else {
                     j++;
                 }
-            }
-        }
 
+            }
+
+        }
 
         return res;
     }
