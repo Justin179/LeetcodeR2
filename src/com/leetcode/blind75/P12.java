05ab1e9d04729934c29ed3059e22859eb5aceb3e
陳君豪 <justin.uob@gmail.com>
2022-03-28 15:14:57 +0800

    blind 10-13 /75

---
+++ src/com/leetcode/blind75/P12.java
@@ -0,0 +1,27 @@
+package com.leetcode.blind75;
+
+public class P12 {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] res = countBits(n);
+        System.out.println(res);
+    }
+    public static int[] countBits(int n) {
+        int[] res = new int[n+1];
+        for (int i = 0; i<=n; i++){
+            int num = countOnes(i);
+            res[i] = num;
+        }
+        return res;
+    }
+
+    private static int countOnes(int i) {
+        int counter = 0;
+        while (i!=0){
+            counter+= (i%2)&1;
+            i>>>=1;
+        }
+        return counter;
+    }
+
+}
