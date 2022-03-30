4e8eb5594c2604dcb64381dc04751441e9cb3f49
陳君豪 <justin.uob@gmail.com>
2022-03-30 22:51:11 +0800

    blind 14-16 /75

---
+++ src/com/leetcode/blind75/P15_p1.java
@@ -0,0 +1,19 @@
+package com.leetcode.blind75;
+
+public class P15_p1 { // 70
+    public static void main(String[] args) {
+
+        climbStairs(3);
+    }
+
+    public static int climbStairs(int n) { // 1 <= n <= 45
+        int[] dp = new int[45];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i<45; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n-1];
+    }
+}
---
+++ src/com/leetcode/blind75/P16.java
@@ -0,0 +1,28 @@
+package com.leetcode.blind75;
+
+import java.util.Arrays;
+
+public class P16 { // lc 322
+    public static void main(String[] args) {
+//        int[] coins = new int[]{1,3,4,5};
+        int[] coins = new int[]{2};
+        int amount = 3;
+        int res = coinChange(coins,amount);
+        System.out.println(res); // 2
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for (int i = 1; i<=amount; i++){
+            for (int coin : coins){ // 1 3 4 5
+                if(i-coin<0)
+                    continue;
+                dp[i] = Math.min(dp[i], 1+dp[i-coin]);
+            }
+        }
+
+        return dp[amount]==amount+1? -1: dp[amount];
+    }
+}
